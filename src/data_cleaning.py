9c53a4667263c82cee54a91f0f30275ebf04595c
riccardomaino <riccardo.maino8@gmail.com>
2026-05-13 18:51:09 +0200

    Completed data cleaning module function

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,28 +1,50 @@
 import pandas as pd
 import numpy as np
 import re
+import os
 
-DATASET_PATH = "data/raw_matches.csv"
+#####---- Constants
+OUTPUT_DIR ="data"
+INPUT_CSV = os.path.join(OUTPUT_DIR, "raw_matches.csv")
+OUTPUT_CSV = os.path.join(OUTPUT_DIR, "cleaned_matches.csv")
+INVALID_SCORE_TOKENS = {"W/O", "RET", "DEF", "ABD", "UNF", "NAN", ""}
+NUMERIC_COLS = ["w_ace", "w_df", "w_svpt", "w_1stIn", "w_1stWon", "w_2ndWon", "w_bpSaved", "w_bpFaced", "w_SvGms",
+            "l_ace", "l_df", "l_svpt", "l_1stIn", "l_1stWon", "l_2ndWon", "l_bpSaved", "l_bpFaced", "l_SvGms",
+            "winner_rank", "winner_rank_points", "loser_rank_points", "loser_rank",
+            "winner_age", "winner_ht", "loser_age", "loser_ht", 
+            "best_of"]
+
 
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
-  # Fill NaN of some columns
-  df[["winner_entry", "loser_entry"]] = df[["winner_entry", "loser_entry"]].fillna("OK")
-  df["surface"] = df["surface"].fillna("Unknown")
-  # Filter out no score matches
-  df = df[df["score"].notna()] 
-  # Filter out NaN values of important stats
-  df = df.dropna(subset=["w_ace", "w_df", "w_svpt", "w_1stIn", "w_1stWon", "w_2ndWon", "w_SvGms", "w_bpSaved", "w_bpFaced"])
-  df = df.dropna(subset=["l_ace", "l_df", "l_svpt", "l_1stIn", "l_1stWon", "l_2ndWon", "l_SvGms", "l_bpSaved", "l_bpFaced"])
+  """
+  Cleans a tennis match dataset by applying standard filtering and data type conversions.
+
+  Args:
+    df (pd.DataFrame): Raw tennis match DataFrame
+
+  Returns:
+    pd.DataFrame: Cleaned DataFrame with filtered rows and properly typed columns.
+  """
+  
+  # Filter out matches with no score
+  df = df[df["score"].notna()]
   # Filter out retirements, walkovers, defaults (stats are skewed/incomplete)
-  df = df[~df["score"].str.contains("RET|W/O|DEF", case=False, na=False)]
-  # Convert tourney_date to datetime
+  df = df[~df["score"].str.contains(INVALID_SCORE_TOKENS, case=False, na=False)].copy()
+  # Handle missing `surface` value
+  df["surface"] = df["surface"].fillna("Unknown")
+  # Convert all the numeric columns to numeric
+  for col in NUMERIC_COLS:
+    df[col] = pd.to_numeric(df[col], errors="coerce")
+  # Convert dates 
   df["tourney_date"] = pd.to_datetime(df["tourney_date"], format="%Y%m%d")
   return df
 
-def build_features(df: pd.DataFrame) -> pd.DataFrame:
-  records = []
-
 if __name__ == "__main__":
-  df = pd.read_csv(DATASET_PATH)
-  
+  df = pd.read_csv(INPUT_CSV)
+  df_cleaned = clean_data(df)
+  df_cleaned.to_csv(OUTPUT_CSV, index=False)
+  print(f"\nSaved {len(df_cleaned):,} matches → {OUTPUT_CSV}")
+  print(f"    Columns : {list(df_cleaned.columns[:10])} ... ({df_cleaned.shape[1]} total)")
+  print(f"    Years   : {df_cleaned["year"].min()} - {df_cleaned["year"].max()}")
+  print(f"    Memory  : {df_cleaned.memory_usage(deep=True).sum() / 1e6:.1f} MB")
   
