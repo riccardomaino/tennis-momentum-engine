a5cade62fd7ddaeac57c4f6dc16912747d3d7691
riccardomaino <riccardo.maino8@gmail.com>
2026-05-13 14:53:08 +0200

    Minor changes in data cleaning

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -6,17 +6,17 @@ DATASET_PATH = "data/raw_matches.csv"
 
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
   # Fill NaN of some columns
-  df[['winner_entry', 'loser_entry']] = df[['winner_entry', 'loser_entry']].fillna('OK')
-  df['surface'] = df['surface'].fillna('Unknown')
+  df[["winner_entry", "loser_entry"]] = df[["winner_entry", "loser_entry"]].fillna("OK")
+  df["surface"] = df["surface"].fillna("Unknown")
   # Filter out no score matches
-  df = df[df['score'].notna()] 
+  df = df[df["score"].notna()] 
   # Filter out NaN values of important stats
-  df = df.dropna(subset=['w_ace', 'w_df', 'w_svpt', 'w_1stIn', 'w_1stWon', 'w_2ndWon', 'w_SvGms', 'w_bpSaved', 'w_bpFaced'])
-  df = df.dropna(subset=['l_ace', 'l_df', 'l_svpt', 'l_1stIn', 'l_1stWon', 'l_2ndWon', 'l_SvGms', 'l_bpSaved', 'l_bpFaced'])
+  df = df.dropna(subset=["w_ace", "w_df", "w_svpt", "w_1stIn", "w_1stWon", "w_2ndWon", "w_SvGms", "w_bpSaved", "w_bpFaced"])
+  df = df.dropna(subset=["l_ace", "l_df", "l_svpt", "l_1stIn", "l_1stWon", "l_2ndWon", "l_SvGms", "l_bpSaved", "l_bpFaced"])
   # Filter out retirements, walkovers, defaults (stats are skewed/incomplete)
-  df = df[~df["score"].str.contains('RET|W/O|DEF', case=False, na=False)]
+  df = df[~df["score"].str.contains("RET|W/O|DEF", case=False, na=False)]
   # Convert tourney_date to datetime
-  df['tourney_date'] = pd.to_datetime(df['tourney_date'], format='%Y%m%d')
+  df["tourney_date"] = pd.to_datetime(df["tourney_date"], format="%Y%m%d")
   return df
 
 def build_features(df: pd.DataFrame) -> pd.DataFrame:
